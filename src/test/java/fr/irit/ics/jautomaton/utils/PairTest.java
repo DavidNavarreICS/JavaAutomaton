355f3895569ea74cb25783f598560203344361f1
navarre <navarre@a17683.ad.ut-capitole.fr>
2020-03-13 10:31:50 +0100

    Enhance tests of the class Pair

--- src/test/java/fr/irit/ics/jautomaton/utils/PairTest.java
+++ src/test/java/fr/irit/ics/jautomaton/utils/PairTest.java
@@ -15,6 +15,7 @@
  */
 package fr.irit.ics.jautomaton.utils;
 
+import java.util.Objects;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -27,28 +28,103 @@ public class PairTest {
     public PairTest() {
     }
 
-    @Test
-    public void testGetFirst() {
+    private static final int DEFAULT_FIRST_ELEMENT = 0;
+    private static final int DEFAULT_DIFFERENT_FIRST_ELEMENT = 1;
+    private static final int DEFAULT_SECOND_ELEMENT = 0;
+    private static final int DEFAULT_DIFFERENT_SECOND_ELEMENT = 1;
+
+    private static Pair<Integer, Integer> getNewPair() {
+        return getNewPair(DEFAULT_FIRST_ELEMENT, DEFAULT_SECOND_ELEMENT);
+    }
+
+    private static Pair<Integer, Integer> getNewPair(Integer e1, Integer e2) {
+        return new Pair<>(e1, e2);
     }
 
     @Test
-    public void testGetSecond() {
+    public void testGetFirstIsSame() {
+        Pair<Integer, Integer> aPair = getNewPair();
+        Integer result = aPair.getFirst();
+        Integer expected = DEFAULT_FIRST_ELEMENT;
+        Assert.assertEquals("getFirst must return the value used to build the pair", expected, result);
     }
 
     @Test
-    public void testHashCode() {
-        Pair<Integer, Integer> aPair = new Pair<>(0, 0);
+    public void testGetSecondIsSame() {
+        Pair<Integer, Integer> aPair = getNewPair();
+        Integer result = aPair.getFirst();
+        Integer expected = DEFAULT_SECOND_ELEMENT;
+        Assert.assertEquals("getSecond must return the value used to build the pair", expected, result);
+    }
+
+    @Test
+    public void testHashCodeNotNull() {
+        Pair<Integer, Integer> aPair = getNewPair();
         Integer result = aPair.hashCode();
-        Integer expected = 0;
-        Assert.assertFalse("Hashcode cannot be null", expected.equals(result));
+        Integer notExpected = 0;
+        Assert.assertFalse("Hashcode cannot be null", notExpected.equals(result));
     }
 
     @Test
-    public void testEquals() {
+    public void testEqualsOtherPairIsSame() {
+        Pair<Integer, Integer> aPair1 = getNewPair();
+        Pair<Integer, Integer> aPair2 = getNewPair();
+        Boolean result1 = aPair1.equals(aPair2);
+        Boolean result2 = aPair1.equals(aPair2);
+        Assert.assertTrue("Two pairs with the same two elements must be equals", result1);
+        Assert.assertTrue("Two pairs with the same two elements must be equals", result2);
     }
 
     @Test
-    public void testToString() {
+    public void testEqualsOtherPairFirstElementIsDifferent() {
+        Pair<Integer, Integer> aPair1 = getNewPair();
+        Pair<Integer, Integer> aPair2 = getNewPair(DEFAULT_DIFFERENT_FIRST_ELEMENT, DEFAULT_SECOND_ELEMENT);
+        Boolean result1 = aPair1.equals(aPair2);
+        Boolean result2 = aPair1.equals(aPair2);
+        Assert.assertFalse("Two pairs with two different elements must not be equals", result1);
+        Assert.assertFalse("Two pairs with two different elements must not be equals", result2);
+    }
+
+    @Test
+    public void testEqualsOtherPairSecondElementIsDifferent() {
+        Pair<Integer, Integer> aPair1 = getNewPair();
+        Pair<Integer, Integer> aPair2 = getNewPair(DEFAULT_FIRST_ELEMENT, DEFAULT_DIFFERENT_SECOND_ELEMENT);
+        Boolean result1 = aPair1.equals(aPair2);
+        Boolean result2 = aPair1.equals(aPair2);
+        Assert.assertFalse("Two pairs with two different elements must not be equals", result1);
+        Assert.assertFalse("Two pairs with two different elements must not be equals", result2);
+    }
+
+    @Test
+    public void testEqualsOtherClassIsDifferent() {
+        Pair<Integer, Integer> aPair1 = getNewPair();
+        Object aPair2 = new Object();
+        Boolean result1 = aPair1.equals(aPair2);
+        Assert.assertFalse("A pair cannot be equals to an object from another class", result1);
+    }
+
+    @Test
+    public void testEqualsNullObjectIsDifferent() {
+        Pair<Integer, Integer> aPair1 = getNewPair();
+        Object aPair2 = null;
+        Boolean result1 = aPair1.equals(aPair2);
+        Assert.assertFalse("A pair cannot be equals to a null object", result1);
+    }
+
+    @Test
+    public void testToStringNotNull() {
+        Pair<Integer, Integer> aPair = getNewPair();
+        String result = aPair.toString();
+        Boolean analyzed = Objects.nonNull(result);
+        Assert.assertTrue("ToString cannot be null", analyzed);
+    }
+
+    @Test
+    public void testToStringNotEmpty() {
+        Pair<Integer, Integer> aPair = getNewPair();
+        String result = aPair.toString();
+        Boolean analyzed = !result.isEmpty();
+        Assert.assertTrue("ToString cannot be empty", analyzed);
     }
 
 }
